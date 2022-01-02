234a8153968477ec4fe864e3347dbc42ed1d63d9
Berk Altıok <berkpw@gmail.com>
2022-01-02 23:16:59 +0300

    feat(landing): create hero component

---
+++ components/home/hero.js
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="h-[640px] bg-blue-500 overflow-hidden relative">
+      <img className="absolute right-0 w-[800px]" src="/images/home/hero.png" alt="Image"/>
+    </div>
+  )
+}
