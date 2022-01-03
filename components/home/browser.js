e93be968e4514b1369a30aa4fd2c402002c17caf
Berk Altıok <berkpw@gmail.com>
2022-01-03 11:57:50 +0300

    feat: create browser component

---
+++ components/home/browser.js
@@ -0,0 +1,12 @@
+export default function Browser({ className, children, style }) {
+  return (
+    <div className={`flex flex-col border bg-white/[.48] border-neutral-1 overflow-hidden backdrop-blur-xl ${className}`} style={style}>
+      <div className="flex-none p-3 flex items-center w-full border-b border-neutral-1 gap-1.5">
+        {[1,2,3].map(i => <div key={i} className="w-2.5 h-2.5 rounded-full bg-neutral-200"/>)}
+      </div>
+      <div className="flex-1 h-full w-full">
+        {children}
+      </div>
+    </div>
+  )
+}
