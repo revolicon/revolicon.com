615c38304fe0ec1b65266d25dac930406c5623e5
Berk Altıok <berkpw@gmail.com>
2022-01-04 01:11:42 +0300

    chore(browser): add flex-col in body

--- components/home/browser.js
+++ components/home/browser.js
@@ -4,7 +4,7 @@ export default function Browser({ className, children, style }) {
       <div className="flex-none p-3 flex items-center w-full border-b border-neutral-1 gap-1.5">
         {[1,2,3].map(i => <div key={i} className="w-2.5 h-2.5 rounded-full bg-neutral-200"/>)}
       </div>
-      <div className="flex-1 h-full w-full">
+      <div className="flex-1 h-full w-full flex flex-col">
         {children}
       </div>
     </div>
