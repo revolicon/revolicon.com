ff266a158668189444f91db9cad1fe8971173dbc
Berk Altıok <berkpw@gmail.com>
2022-01-12 12:54:27 +0300

    chore: update package and icons

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,9 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
-        <link rel="stylesheet" href="https://i.icomoon.io/public/temp/74623cbca0/Revolicon-Line/style.css"/>
-        <link rel="stylesheet" href="https://i.icomoon.io/public/temp/6e521450a0/Revolicon-Solid/style.css"/>
-        <link rel="stylesheet" href="https://i.icomoon.io/public/temp/f7c78af9b8/Revolicon-Brands/style.css"/>
+        <link rel="stylesheet" href="https://i.icomoon.io/public/temp/42504d5b7e/Revolicon-Line/style.css"/>
+        <link rel="stylesheet" href="https://i.icomoon.io/public/temp/115b7171af/Revolicon-Solid/style.css"/>
+        <link rel="stylesheet" href="https://i.icomoon.io/public/temp/9b14bf567d/Revolicon-Brands/style.css"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
       </Head>
       <body>
